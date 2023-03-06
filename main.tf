1800d99c08515a5e9077eb6b7865364cfb0b23ab
bmanoj18 <120033688+bmanoj18@users.noreply.github.com>
2023-03-06 13:13:42 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "google_compute_instance" "testing" {
+  name         = "testing"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+  
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = terraform-testing@fourth-arena-379102.iam.gserviceaccount.com
+    scopes = ["cloud-platform"]
+  }
+}
